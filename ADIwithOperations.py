0dc511b190e5369a210d4d85e920073de30ced02
pragyapriya-18 <pragyapriya.530@gmail.com>
2025-04-17 23:36:47 +0530

    Added

---
+++ ADIwithOperations.py
@@ -0,0 +1,13 @@
+class Date:
+    def __init__(self):
+        self.month = 0
+        self.year = 0
+
+date1 = Date()
+date1.day = 2
+date1.month = 8
+date1.year = 1990
+
+print("day=", date1.day)
+print("month=", date1.month)
+print("year=", date1.year)
